38a72412cfdbe166a23ace52cd8144bddfd287e2
Yogi Prasetya <yogiprsetya@gmail.com>
2019-11-08 16:10:30 +0700

    update about

--- src/component/about.js
+++ src/component/about.js
@@ -6,17 +6,10 @@ export default class About extends Component {
     return (
       <View style={{ margin: 15 }}>
         <Text style={{ marginBottom: 10 }}>
-          Sampai dengan 30 September 2019, total jumlah penyelenggara fintech
-          terdaftar dan berizin adalah sebanyak 127 perusahaan.
-        </Text>
-        <Text style={{ marginBottom: 10 }}>
-          Terdapat penambahan enam penyelenggara fintech yang sebelumnya sudah
-          terdaftar menjadi berizin yaitu, Modalku, KTA Kilat, Kredit Pintar,
-          Maucash, Finmas, dan KlikACC.
+          Kamus musik adalah direktori offline kumpulan istilah dalam dunia musik berbahasa Indonesia.
         </Text>
         <Text>
-          OJK mengimbau masyarakat untuk menggunakan jasa penyelenggaran fintech
-          peer to peer lending yang sudah terdaftar/berizin dari OJK.
+          Berbagai istilah dikumpulkan dari berbagai sumber.
         </Text>
       </View>
     )
